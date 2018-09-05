6c488b8c4fc6ab7b06d5f52e6ed7ebfa176e0378
Elizabeth Roten <roten.elizabeth@gmail.com>
2018-09-05 10:07:49 -0500

    specify path for testing on this machine

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
 library(testthat)
-library(rintrojs)
+library(devtools)
+devtools::load_all('/Users/RotenEE/OneDrive - Metropolitan Council/packageForks/rintrojs')
 
 test_check("rintrojs")
+testthat::test_dir(path = '/Users/RotenEE/OneDrive - Metropolitan Council/packageForks/rintrojs/tests')
